39b6d6e0c7011e0c5327fd4ec87adae1d9755999
sonerich <sonerichpham@protonmail.com>
2021-05-22 22:45:03 +0200

    customized the tty

--- .config/zsh/autoload/32-zsh-syntax-highlighting.zsh
+++ .config/zsh/autoload/32-zsh-syntax-highlighting.zsh
@@ -1,6 +1,13 @@
 # highlighting
 typeset -A ZSH_HIGHLIGHT_STYLES
-ZSH_HIGHLIGHT_STYLES[alias]='fg=magenta,bold'
-ZSH_HIGHLIGHT_STYLES[default]='fg=cyan'
-ZSH_HIGHLIGHT_STYLES[autodirectory]='fg=cyan,underline'
-ZSH_HIGHLIGHT_STYLES[unknown-token]='fg=yellow,underline'
+if [ "$TERM" != "linux" ]; then
+	ZSH_HIGHLIGHT_STYLES[alias]='fg=magenta,bold'
+	ZSH_HIGHLIGHT_STYLES[default]='fg=cyan'
+	ZSH_HIGHLIGHT_STYLES[autodirectory]='fg=cyan,underline'
+	ZSH_HIGHLIGHT_STYLES[unknown-token]='fg=yellow,underline'
+else
+	ZSH_HIGHLIGHT_STYLES[alias]='fg=magenta'
+	ZSH_HIGHLIGHT_STYLES[default]='fg=cyan'
+	ZSH_HIGHLIGHT_STYLES[autodirectory]='fg=green'
+	ZSH_HIGHLIGHT_STYLES[unknown-token]='fg=red'
+fi
--- .config/zsh/autoload/90-power10k.zsh
+++ .config/zsh/autoload/90-power10k.zsh
@@ -1,5 +1,6 @@
 # different prompts for tty and terminal
 if [ "$TERM" = "linux" ]; then
+	echo -en "\e]P0232323"
 	[[ ! -f ~/.config/zsh/.p10k_tty.zsh ]] || source ~/.config/zsh/.p10k_tty.zsh
 else
 	[[ ! -f ~/.config/zsh/.p10k_terminal.zsh ]] || source ~/.config/zsh/.p10k_terminal.zsh
